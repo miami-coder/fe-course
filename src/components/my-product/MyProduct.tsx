6ea3a018fc7592233ff446f0a878f2e4af096dc7
miami-coder <defenderxz229@gmail.com>
2025-11-29 22:30:22 +0200

    Lesson 1

--- src/components/my-product/MyProduct.tsx
+++ src/components/my-product/MyProduct.tsx
@@ -2,15 +2,16 @@
 import type {IProduct} from "../models/IProduct.ts";
 import type {FC} from "react";
 
-type MyPropsType = {
-    product: IProduct;
-}
+// type MyPropsType = {
+//     product: IProduct;
+// }
 
-const MyProduct: FC<MyPropsType> = ({product}) => {
+const MyProduct: FC<IProduct> = ({title, price, image}) => {
+    // const {title, price, image} = product;
     return (
         <div>
-            <h2>{product.title}. {product.price} uah</h2>
-            <img src={product.image} alt={product.title}/>
+            <h2>{title}. {price} uah</h2>
+            <img src={image} alt={title}/>
         </div>
     );
 };
