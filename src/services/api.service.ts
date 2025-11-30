3e2836dadb0fe8da783fef25bc569adb550de55b
miami-coder <defenderxz229@gmail.com>
2025-11-30 16:37:52 +0200

    .env

--- src/services/api.service.ts
+++ src/services/api.service.ts
@@ -1,11 +1,12 @@
 import type {IUser} from "../models/IUser.ts";
 
+
 export const getUsers = async ():Promise<IUser[]> => {
-    return await fetch('https://jsonplaceholder.typicode.com/users')
+    return await fetch(import.meta.env.VITE_API_URL)
         .then((response) => response.json());
 }
 
 export const getUser = async (id: string):Promise<IUser> => {
-    return await fetch('https://jsonplaceholder.typicode.com/users' + id)
+    return await fetch(import.meta.env.VITE_API_URL + '/' + id)
         .then((response) => response.json());
 }
