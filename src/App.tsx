1444c876e0621e73417c3dd5f869a0a33ef0b44f
miami-coder <defenderxz229@gmail.com>
2025-11-30 16:10:04 +0200

    useEffect

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import './App.css'
 import UsersComponent from "./components/users-component/UsersComponent.tsx";
 
 const App = () => {
-    console.log('app');
-
-
-
-
   return (
       <>
         <UsersComponent/>
