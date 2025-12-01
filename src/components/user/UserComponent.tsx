981f990d7f17e0037c3ebc3a94674d86b9729c9d
miami-coder <defenderxz229@gmail.com>
2025-12-01 18:02:11 +0200

    state forward

--- src/components/user/UserComponent.tsx
+++ src/components/user/UserComponent.tsx
@@ -1,15 +1,20 @@
 import type {FC} from "react";
 import type {IUser} from "../../models/IUser.ts";
+import {Link, useNavigate} from "react-router-dom";
 
 type UserTypeProps = {
     item: IUser;
 }
 const UserComponent: FC<UserTypeProps> = ({item}) => {
+    const navigate = useNavigate();
+    const handleOnClick = () => {
+        navigate('details', {state:item})
+    }
     return (
         <div>
-            {
-                item.username
-            }
+            <Link to={'details'} state={item} >{item.username}</Link>
+
+            <button onClick={handleOnClick}>go to details</button>
         </div>
     );
 };
