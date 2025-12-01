2c1f14a62f966ba8eafa41b1d3c44424162bb4f6
miami-coder <defenderxz229@gmail.com>
2025-12-01 21:19:19 +0200

    hw3 task1 part2

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,22 @@
 import {createRoot} from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import {routes} from "./router/routes.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import UsersComponent from "./components/pages/UsersComponent.tsx";
+import PostComponent from "./components/pages/PostComponent.tsx";
+import CommentsComponent from "./components/pages/CommentsComponent.tsx";
+import ProductsComponent from "./components/pages/ProductsComponent.tsx";
+import App from "./App.tsx";
 
 
 createRoot(document.getElementById('root')!).render(
-    <RouterProvider router={routes}/>
+    <BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<App/>}>
+                <Route path={'users'} element={<UsersComponent/>}/>
+                <Route path={'posts'} element={<PostComponent/>}/>
+                <Route path={'comments'} element={<CommentsComponent/>}/>
+                <Route path={'products'} element={<ProductsComponent/>}/>
+            </Route>
+        </Routes>
+    </BrowserRouter>
 )
--- src/router/routes.tsx
+++ src/router/routes.tsx
@@ -1,9 +1,9 @@
 import {createBrowserRouter} from "react-router-dom";
 import App from "../App.tsx";
-import UsersComponent from "../components/UsersComponent.tsx";
-import PostComponent from "../components/PostComponent.tsx";
-import CommentsComponent from "../components/CommentsComponent.tsx";
-import ProductsComponent from "../components/ProductsComponent.tsx";
+import UsersComponent from "../components/pages/UsersComponent.tsx";
+import PostComponent from "../components/pages/PostComponent.tsx";
+import CommentsComponent from "../components/pages/CommentsComponent.tsx";
+import ProductsComponent from "../components/pages/ProductsComponent.tsx";
 
 export const routes = createBrowserRouter([
     {
