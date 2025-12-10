0cd373f52046321331b308a008ebcd10024c6c65
miami-coder <defenderxz229@gmail.com>
2025-12-10 13:29:21 +0200

    uncontrolled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import FormComponents from "./components/FormComponents.tsx";
 
 function App() {
 
   return (
       <>
-
+        <FormComponents/>
       </>
   )
 }
---
+++ src/components/FormComponents.tsx
@@ -0,0 +1,12 @@
+const FormComponents = () => {
+    return (
+        <div>
+            <form>
+                <input type="text" name={'username'}/>
+                <input type="text" name={'password'}/>
+            </form>
+        </div>
+    );
+};
+
+export default FormComponents;
