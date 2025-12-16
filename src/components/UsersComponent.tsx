1d0e0eacbac5a97116cb7a9e47d345c0fb16006a
miami-coder <defenderxz229@gmail.com>
2025-12-16 13:45:29 +0200

    useMemo

--- src/components/UsersComponent.tsx
+++ src/components/UsersComponent.tsx
@@ -1,23 +1,31 @@
 import {UserComponent} from "./UserComponent.tsx";
-import {useCallback, useEffect, useState} from "react";
+import {useCallback, useEffect, useMemo, useState} from "react";
 
 const UsersComponent = () => {
+    console.log('users')
+
     const [user, setUser] = useState()
+
+    const arr: number[] = useMemo(() => {
+        return [11, 22, 33];
+    }, []);
 
     const foo = useCallback(() => {
         console.log('test')
     }, [])
-
-    console.log('users')
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/users')
             .then((response) => response.json())
             .then((value) => setUser(value))
+
+        return () => {
+            console.log('unsubscribe');
+        }
     }, []);
     return (
         <div>
             users component
-            <UserComponent foo={foo}/>
+            <UserComponent foo={foo} arr={arr}/>
         </div>
     );
 };
